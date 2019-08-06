ec1035635364f5a1e40b2c22cfa838b1f84db045
Ahmad Ragab <a.ragab2012@gmail.com>
2019-08-06 21:05:13 +0200

    remove default view controller and put new one to show movies list in a table view

--- AMDB/View Layer/MoviesListViewController.swift
+++ AMDB/View Layer/MoviesListViewController.swift
@@ -1,17 +1,16 @@
 //
-//  ViewController.swift
+//  MoviesListViewController.swift
 //  AMDB
 //
-//  Created by Ahmad Ragab on 8/5/19.
+//  Created by Ahmad Ragab on 8/6/19.
 //  Copyright © 2019 Ahmad Ragab. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-
