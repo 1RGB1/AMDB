210f30b11143a7394f8faaf8aadd6d49ec4cbb0d
Ahmad Ragab <a.ragab2012@gmail.com>
2019-08-07 03:57:45 +0200

    Adding collection view cell

---
+++ AMDB/View Layer/Movies List/CompanyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CompanyCollectionViewCell.swift
+//  AMDB
+//
+//  Created by Ahmad Ragab on 8/7/19.
+//  Copyright © 2019 Ahmad Ragab. All rights reserved.
+//
+
+import UIKit
+
+class CompanyCollectionViewCell : UICollectionViewCell {
+    
+}
